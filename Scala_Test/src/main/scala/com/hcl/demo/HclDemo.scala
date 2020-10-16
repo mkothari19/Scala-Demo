a84c4a025ea168c15906bf838b498f31cb42134f
Maneesh Kothari <manishkothari1982@gmail.com>
2020-10-16 11:21:21 +0530

    Update source

--- Scala_Test/src/main/scala/com/hcl/demo/HclDemo.scala
+++ Scala_Test/src/main/scala/com/hcl/demo/HclDemo.scala
@@ -1,5 +1,7 @@
 package com.hcl.demo
 
+import java.util.Scanner
+
 object HclDemo {
   
   def printNumber(n:Int,sep:String):String = {
@@ -11,10 +13,14 @@ object HclDemo {
   def calculateAOT(base:Double,height:Double) = (base*height)/2
   
  def main(args: Array[String]): Unit = {
-    
-    
-    println("Code that prints the numbers 1,2,3,4,5,6,7,8,9,10 \n"+printNumber(10,","))
-    println("Code that calculates the area of a triangle \n"+calculateAOT(5.0,10.0))
+     println("Code that prints the numbers 1,2,3,4,5,6,7,8,9,10 \n Please input number \n")
+    val input =new Scanner(System.in)
+    println("OUT PUT="+printNumber(input.nextInt(),","))
+   println("\nInput to calulate area of a triangle \n Please enter base and hight\n")
+    val scanner =new Scanner(System.in)
+    val base =scanner.nextDouble()
+    val hight=scanner.nextDouble()
+    println("Area of a triangle="+calculateAOT(base,hight))
  } 
   
 }
